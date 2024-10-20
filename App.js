48f9aa9aa7b950dbdce21496fe93fcefc153414a
ElBrayanA <ffrfdke86@gmail.com>
2024-10-20 13:52:53 -0700

    Aplicacion de mapas con api de google

--- App.js
+++ App.js
@@ -1,12 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as Location from 'expo-location'
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker, Polyline} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import { KEY } from '@env';
 
 export default function App() {
+
+  const [origin, setOrigin] = React.useState({
+    latitude:32.437725,
+    longitude: -114.716356
+  });
+
+const [destination, setDestination] = React.useState({
+  latitude:32.437725,
+  longitude: -114.716356
+});
+
+React.useEffect(() =>{
+getLocationPermission();
+},[])
+
+async function getLocationPermission(){
+  let {status} = await Location.requestForegroundPermissionsAsync();
+  if(status !== "granted"){
+    alert("Permission denied");
+    return;
+  }
+  let location = await Location.getCurrentPositionAsync({});
+  const current ={
+    latitude: location.coords.latitude,
+    longitude: location.coords.longitude
+  }
+  setOrigin(current);
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude:origin.latitude,
+        longitude: origin.longitude,
+        latitudeDelta:0.99,
+        longitudeDelta:0.04
+      }}> 
+      
+      <Marker 
+      draggable
+      coordinate={origin}
+      onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}/>
+
+       <Marker 
+      draggable
+      coordinate={destination}
+      onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)}/>
+
+      <MapViewDirections
+      origin={origin}
+      destination={destination}
+      apiKey={KEY}
+      strokeColor='blue'
+      strokeWidth={2}/>
+    
+      </MapView>
     </View>
   );
 }
@@ -18,4 +76,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map:{
+    width: '100%',
+    height: '100%'
+  }
 });
